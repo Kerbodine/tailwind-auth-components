0814756001d907ad58bc567e914201317e05a4fd
Michael Tong <michaelyuyangtong@gmail.com>
2022-03-07 13:21:23 +0800

    Added input field component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import InputFloatingLabel from "./components/inputs/InputFloatingLabel";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="w-full h-screen grid place-items-center bg-gray-600">
+      <div className="w-64 h-64 bg-white rounded-2xl shadow-lg p-4">
+        <InputFloatingLabel
+          id="email"
+          type="email"
+          placeholder="Email Address"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
